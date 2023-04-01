107c53a53778ec73dc0a4ac8866e3278829145da
João Gabriel Z.B <71906858+JoaoGabrielBertolucci@users.noreply.github.com>
2023-04-01 14:23:29 -0300

    definição de rota para o componente

--- caculator/src/app/app-routing.module.ts
+++ caculator/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
 
 const routes: Routes = [ 
+  { path: 'calculadora', component: CalculadoraComponent }
 ];
 
 @NgModule({
