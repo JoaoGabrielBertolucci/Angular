667b45d065e92fa514b4551a11709a4b1fa9aff2
João Gabriel Z.B <71906858+JoaoGabrielBertolucci@users.noreply.github.com>
2023-04-01 18:33:46 -0300

    atualização 01/04/23
    foi criado e editado algumas linhas de typescript e houve alteração em algumas outras  linhas dos arquivos html

--- caculator/src/app/calculadora/calculadora.component.ts
+++ caculator/src/app/calculadora/calculadora.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./calculadora.component.css']
 })
 export class CalculadoraComponent {
+  
+  num1 : number = 0; 
+  num2 : number = 0;
+  soma : number = 0;
+  Subtrair: number = 0;
+  mult: number = 0;
+  divisao: number = 0;
+  RestoDiv: number = 0;
+
+   somar(){
+      this.soma = this.num1 + this.num2;
+   }
+
+  subtrair(){
+    this.Subtrair = this.num1 - this.num2;
+  }
+
+  multiplicar(){
+    this.mult = this.num1 * this.num2;
+  }
+
+  dividir(){
+    this.mult = this.num1 / this.num2;
+  }
+
+  restodiv(){
+    this.RestoDiv = this.num1 % this.num2;
+  }
 
 }
+
